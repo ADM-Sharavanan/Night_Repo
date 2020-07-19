6ac8f607822de4528d2e121a15adcd277a501170
SharavananDemo <ishasharavanan@gmail.com>
2020-07-19 23:00:03 +0530

    Gn_latest

---
+++ src/study_purpose/Good_night_demo.java
@@ -0,0 +1,13 @@
+package study_purpose;
+
+public class Good_night_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Latest Good night");
+
+	}
+
+}
